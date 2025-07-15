0f74ef5fd4ad9632c177c9d592f4129b21e5f828
quincyvanstenus <quincyvanstenus@crisp.nl>
2025-07-15 15:34:45 +0200

    Fixed right prompt charachter

--- fish/functions/fish_right_prompt.fish
+++ fish/functions/fish_right_prompt.fish
@@ -4,30 +4,30 @@ function fish_right_prompt
         set -l icon ""
         switch (uname)
             case Darwin
-                set icon ' '
+                set icon ''
             case Linux
                 if type -q lsb_release
                     set -l distro (lsb_release -i | cut -f2)
                     switch $distro
                         case Ubuntu
-                            set icon ' '
+                            set icon '' 
                         case Debian
-                            set icon ' '
+                            set icon ''
                         case '*'
-                            set icon ' '
+                            set icon ''
                     end
                 else
-                    set icon ' '
+                    set icon ''
                 end
         end
         set right_prompt (prompt_hostname) $icon
     else if test -f /.dockerenv -o -f /run/.containerenv
-        set right_prompt '  '
+        set right_prompt ''
     end
-    if test -n $right_prompt
+    if test -n "$right_prompt"
         # Makes it so the right prompt is on top line
         tput sc; tput cuu1; tput cuf 2
-        echo -n $dark_grey $right_prompt
+        echo -n $dark_grey $right_prompt '  '
         tput rc
     end
 end
